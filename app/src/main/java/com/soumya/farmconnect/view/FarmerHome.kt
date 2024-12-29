18a95d75de517baae4a5e72a02855e47d7d5c6d6
P Soumya Sundar Subudhi <p.soumyasundars@gmail.com>
2024-12-30 02:25:31 +0530

    updated home for farmer

--- app/src/main/java/com/soumya/farmconnect/view/FarmerHome.kt
+++ app/src/main/java/com/soumya/farmconnect/view/FarmerHome.kt
@@ -2,26 +2,20 @@ package com.soumya.farmconnect.view
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.Row
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,7 +28,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.soumya.farmconnect.R
 import com.soumya.farmconnect.ui.components.BaseBackground
